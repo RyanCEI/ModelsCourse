e569a619b8b180382b1ffb99ae2a0bde5ca8f9db
Ryan Dowling <ryan@ceics.com>
2020-05-13 11:48:06 -0500

    Pre automapper checkpoint

---
+++ src/JurisTempus/ViewModels/ClientViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JurisTempus.ViewModels
+{
+  public class ClientViewModel
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ContactName { get; set; }
+    public string Phone { get; set; }
+  }
+}
