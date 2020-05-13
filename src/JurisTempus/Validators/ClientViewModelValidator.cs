92d0695b83710fbf58b03b23ba7f137630d2d882
Ryan Dowling <ryan@ceics.com>
2020-05-13 14:41:26 -0500

    Complex valdiator

--- src/JurisTempus/Validators/ClientViewModelValidator.cs
+++ src/JurisTempus/Validators/ClientViewModelValidator.cs
@@ -15,6 +15,12 @@ namespace JurisTempus.Validators
                           .MinimumLength(5)
                           .MaximumLength(100);
       RuleFor(c => c.ContactName).MaximumLength(50);
+      When(c => !string.IsNullOrEmpty(c.Phone) || !string.IsNullOrEmpty(c.ContactName),
+        () =>
+        {
+          RuleFor(c => c.Phone).NotEmpty().WithMessage("Phone cannot be empty if contact is specified");
+          RuleFor(c => c.ContactName).NotEmpty().WithMessage("Contact cannot be empty if phone is specified");
+        });
     }
   }
 }
